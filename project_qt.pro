6365792d4d7a32ae2e02c17898af853975cb0af9
MattDupraz <matthew.dupraz@gmail.com>
2020-04-30 16:47:54 +0200

    J'ai oublie de tout ajouter au commit precedent

--- project_qt.pro
+++ project_qt.pro
@@ -9,9 +9,12 @@ QT += core gui opengl widgets
 
 SOURCES += src/qt/main_qt.cpp \
            src/qt/gl_widget.cpp \
-           src/qt/view_opengl.cpp
+           src/qt/view_opengl.cpp \
+			  src/qt/gl_mesh.cpp
 HEADERS += src/qt/gl_widget.h \
            src/qt/view_opengl.h \
-           src/qt/vertex_shader.h
+           src/qt/vertex_shader.h \
+			  src/qt/gl_mesh.h \
+			  src/qt/vertex.h
 
 RESOURCES += res/resources.qrc
--- res/vertex_shader.glsl
+++ res/vertex_shader.glsl
@@ -1,6 +1,6 @@
 // VERTEX SHADER
 
-attribute vec3 aVertex;
+attribute vec3 aPos;
 attribute vec3 aColor;
 attribute vec3 aNormal;
 
@@ -8,13 +8,13 @@ uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model;
 
+varying vec3 pos;
 varying vec3 color;
 varying vec3 normal;
-varying vec3 pos;
 
 void main() {
-	gl_Position = projection * view * model * vec4(aVertex, 1.0);
+	gl_Position = projection * view * model * vec4(aPos, 1.0);
 	color = aColor;
 	normal = vec3(model * vec4(aNormal, 1.0));
-	pos = vec3(model * vec4(aVertex, 1.0));
+	pos = vec3(model * vec4(aPos, 1.0));
 }
