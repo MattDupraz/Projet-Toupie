e91fb798c233f6b7e996c6b29c291ff99793a6b9
Benjamin <bschmid@INTRANET.EPFL.CH@IC-IN-SC-65.intranet.epfl.ch>
2020-05-14 11:31:48 +0200

    re

--- src/common/Integrator_RungeKutta.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "integrator.h"
-
-// Methode Runge-Kutta
-class RungeKuttaIntegrator : public Integrator{
-	public:
-
-		virtual void evolve(Top& top, double dt) override;
-};
